a189c4dc666523e7eea74a317cfc7b304f0e667d
jasim <jasim@zitelab.dk>
2018-02-26 13:09:15 +0600

    functional task done

--- src/app/store/reducers/setting.reducer.ts
+++ src/app/store/reducers/setting.reducer.ts
@@ -16,7 +16,8 @@ export interface SettingState {
 export const initialState: SettingState = {
     language: 'ba',
     theme:'white',
-    fontSize:'18px'
+    fontSize:'18px',
+    video:{}
 };
 
 export function settingReducer(
